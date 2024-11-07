3d5c2ee5a78de6fd3d2758141ea5e9cb1aa5e2ca
techzik323 <techzik323@gmail.com>
2024-11-08 00:57:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "rrxU2LKQ#ihXNd79w53ZO1cp-FrvO-pFMEAZ1FPV0U79oOSrvI6Y",
 MONGODB: process.env.MONGODB || "enter mongodb url",
 };
